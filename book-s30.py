ec94c85c2a36af5e808870bb783aba5b4bcceb8e
SandmanMaybeTe23 <billt.burlin@elev.ga.ntig.se>
2024-09-10 15:09:41 +0200

    lektioner

---
+++ book-s30.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+##i = 19
+##print(f"resultat:{i}")
+##svar = input("skriv ett heltal:")
+##x = float(svar)
+##i= 19
+##print(f"Resultat: {i}")
+##j= 107
+##print(f"Resultat:{j:5}")
+##k = 4
+##print(f"Resultat:{k:03}")
+##print(f"i={i} och j ={j} ")
+##y = x * x
+##print (f"tatet i kvadrat är,{y:.2f}")
