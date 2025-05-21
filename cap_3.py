4ed78de3d70ba10a1e203fcaeef79b1b41ab5a38
SandmanMaybeTe23 <billt.burlin@elev.ga.ntig.se>
2025-05-21 14:24:14 +0200

    Commit all

---
+++ cap_3.py
@@ -0,0 +1,12 @@
+##money=int(input("How mush money you got"))
+## this code makes it so you can pick how mush money you got 
+##if money >=100:
+    ##print("you got that 100 dolor bill")
+## this code make so if you got more or less money it will give you the two options 
+##elif money < 100:
+    ##print(f"you only got {money} bills What ")
+
+
+time_on_call=int(input)("Please state your call time")
+
+
